f255ff29b7d7e89e168f3933ed1c765fe8e351c6
Nicolas Giard <github@ngpixel.com>
2026-02-23 18:19:49 -0500

    chore: fix escape characters in pull-latest-image.sh

--- .devcontainer/pull-latest-image.sh
+++ .devcontainer/pull-latest-image.sh
@@ -4,5 +4,5 @@ echo "Pulling latest rfced-base image..."
 docker pull ghcr.io/rfc-editor-drafts/rfced-base:latest
 echo
 echo "----------------------------------"
-echo "In Visual Studio Code, open the Command Palette (F1) and choose "Rebuild Container Without Cache" to use the updated image."
+echo "In Visual Studio Code, open the Command Palette (F1) and choose \"Rebuild Container Without Cache\" to use the updated image."
 echo "----------------------------------"
